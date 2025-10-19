e5837445a14dd7843b66678de120904d9f8990c3
max <DubrovinMax1256@gmail.com>
2025-10-19 15:42:21 +0300

    add user model

---
+++ app/core/models/user.py
@@ -0,0 +1,18 @@
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyBaseUserDatabase
+    )
+from core.types.user_id import userId
+from .base import Base
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:   
+    from sqlalchemy.ext.asyncio import AsyncSession
+    
+class User(Base,SQLAlchemyBaseUserTable[userId]):
+    
+    
+    @classmethod
+    def get_db(cls,session:"AsyncSession"):
+        return SQLAlchemyBaseUserDatabase(session,cls)
+
